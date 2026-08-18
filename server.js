aa7a25cecdcfdedb88750d148059b2d54bee86b4
Tharusha69 <150590598+Tharusha69@users.noreply.github.com>
2026-08-18 21:09:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -52,17 +52,14 @@ db.serialize(() => {
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-// පවතින middleware එක වෙනුවට මෙය යොදන්න (uploadsDir භාවිතයෙන්):
 app.use("/uploads", (req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
   next();
-}, express.static(uploadsDir, {
-  maxAge: '7d',          // දින 7ක් Cache කරගනී
-  etag: true,
-  lastModified: true
-}));
+}, express.static(uploadsDir));
 app.use(express.static(__dirname));
+
+
 
 
 // Multer configuration
